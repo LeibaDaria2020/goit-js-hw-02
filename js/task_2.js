3de2471f7aecfb37642aba06d468eeb6e3a8329a
LeibaDaria2020 <leibadaria@gmail.com>
2020-12-08 15:31:36 +0200

    commit

---
+++ js/task_2.js
@@ -0,0 +1,23 @@
+const calculateEngravingPrice = function (message, pricePerWord) {
+  let count = 0
+
+  console.log(message)
+
+  message = message.split(" ")
+  console.log(message)
+  for (let i = 0; i < message.length; i++) {
+    if (message[i] != " ") {
+      count += 1 * pricePerWord
+    }
+  }
+
+  return count
+}
+
+console.log(calculateEngravingPrice("Proin sociis natoque et magnis parturient montes mus", 10)) // 80
+
+console.log(calculateEngravingPrice("Proin sociis natoque et magnis parturient montes mus", 20)) // 160
+
+console.log(calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)) // 240
+
+console.log(calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)) // 120
